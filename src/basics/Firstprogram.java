dd0b44258e737ce8a7e482cf7212ee98f6389446
kavin <kavichandrankrs18@gmail.com>
2022-08-17 00:15:45 -0400

    first program hello world

---
+++ src/basics/Firstprogram.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+       System.out.print("welcome");
+	}
+
+}
