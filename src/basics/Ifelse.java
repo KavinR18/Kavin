b85435afe3eabd33031df10fbfb8af5cabdd3cb6
kavin <kavin@kavin>
2022-08-31 09:26:17 +0530

    increment and decrement if else elgibile for loan even number or odd number positive or negative number

---
+++ src/basics/Ifelse.java
@@ -0,0 +1,54 @@
+package basics;
+
+public class Ifelse {
+   public static void main(String[]args){
+	   
+	   
+//given person is elgible for loan
+	   
+	   int age=87;
+	   
+	  if(age>=18 && age <=60)
+	   {
+		   System.out.println("loan eligible");
+		   }
+	   
+	   else
+	   
+	   
+	   {
+		   System.out.println("No loan");
+		   }
+	  
+	
+//given num is odd or even  
+	  
+	   int num=8;
+	   
+	   if(num%2==0) {
+		   System.out.println("its even number");}
+		   else
+		   {
+	   System.out.println("its odd number");
+		   }
+	   
+	   
+	   
+ //whether its positive or negative
+	     
+	   int a=-18;
+	   if(a>=0)
+	   {
+		   System.out.println("positive"); 
+		   }
+	   else
+	   {
+		   System.out.println("negative");
+		   }
+	   
+	   
+	   
+	   
+	   }   
+	  
+}
---
+++ src/basics/IncrementDecrement.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=5;
+int a=x++;  //post increment
+
+   System.out.println(a);
+   System.out.println(x);
+	}
+
+}
