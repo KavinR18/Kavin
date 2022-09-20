12a401292acb310effdb3a66423d5de56e44c75b
kavin <kavichandrankrs18@gmail.com>
2022-09-20 09:51:40 +0530

    PATTERN PROGRAM BOX RIGHT ANGLED TRIANGLE INCREASING TRIANGLE DECREASING TRIANGLE DIAMOND DAIGONAL HOLLOW BOX

--- src/basics/Pattern5.java
+++ src/basics/Pattern5.java
@@ -1,18 +1,24 @@
 package basics;
 
-public class Pattern {
+public class Pattern5 {
 
 	public static void main(String[] args) {
+		
 		int n = 5;
 		for (int i = 1; i <= n; i++)
 		{
 		     for (int j = 1; j <= n; j++)
-		     {
-				System.out.print("* ");
+		     {if(i==1||i==n || j==1||j==n)
+		     
+		    		 
+		    		System.out.print("*");	 
+		    	 else
+		    	 System.out.print(" ");
 			}
 			System.out.println();
 
-		}
+
 	}
 
+}
 }
